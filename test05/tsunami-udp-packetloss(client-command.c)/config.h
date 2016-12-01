9770e2ff38de8577a60a9968797cd7b7a9370c2d
yws3073 <yws3073@naver.com>
2016-12-01 12:03:00 +0900

    Update zipfile

---
+++ test05/tsunami-udp-packetloss(client-command.c)/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.in by configure.  */
+/* config.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "tsunami"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "tsunami"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "tsunami 1.1b43"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "tsunami"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.1b43"
+
+/* Version number of package */
+#define VERSION "1.1b43"
